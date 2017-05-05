7df1045c776224b4bd922d0f3b86e3bf124ab327
Hugo Carneiro <hcarneiro@weboo.biz>
2017-05-05 13:02:38 +0100

    Added Swipe to Save

--- js/build.js
+++ js/build.js
@@ -1,11 +1,22 @@
-$('.linked[data-thumb-l-item-id]').click(function (event) {
-    event.preventDefault();
+$('.linked[data-thumb-l-item-id]').click(function(event) {
+  event.preventDefault();
 
-    var data = Fliplet.Widget.getData($(this).parents('[data-list-thumb-l-id]').data('list-thumb-l-id'));
+  var data = Fliplet.Widget.getData($(this).parents('[data-list-thumb-l-id]').data('list-thumb-l-id'));
+  var itemData = _.find(data.items, {
+    id: $(this).data('thumb-l-item-id')
+  });
 
-    var itemData = _.find(data.items,{id: $(this).data('thumb-l-item-id')});
+  if (!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
+    Fliplet.Navigate.to(itemData.linkAction);
+  }
+});
 
-    if(!_.isUndefined(itemData) && (!_.isUndefined(itemData.linkAction) && !_.isEmpty(itemData.linkAction))) {
-        Fliplet.Navigate.to(itemData.linkAction);
-    }
+$('[data-list-thumb-l-id]').each(function() {
+  var data = Fliplet.Widget.getData($(this).attr('data-list-thumb-l-id'));
+  if (data.swipeToSave) {
+    window.ui = window.ui || {};
+    window.ui['swipeSavedList' + $(this).attr('data-list-thumb-l-uuid')] = new SwipeSaveList(this, {
+      savedListLabel: data.swipeToSaveLabel || 'My list'
+    });
+  }
 });
